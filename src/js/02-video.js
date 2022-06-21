e52dde4e9e471ef4ac6696c703db1686f29a55c3
RoRomario360 <fotoromario@gmail.com>
2022-06-21 22:23:44 +0300

    commit-3-task-throutle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(evt) {
+  localStorage.setItem('videoplayer-current-time', evt.seconds);
+}
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+const lastPosition = localStorage.getItem('videoplayer-current-time');
+if (lastPosition) {
+  player.setCurrentTime(lastPosition);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const formFeedback = document.querySelector('.feedback-form');
+formFeedback.addEventListener('input', onFormData);
+formFeedback.addEventListener('submit', onSubmitForm);
 
+const formData = {};
+initForm();
+
+function onFormData(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+function onSubmitForm(evt) {
+  console.log(formData);
+  evt.preventDefault();
+  formFeedback.reset();
+  localStorage.clear();
+}
+
+function initForm() {
+  let inputFormValues = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (inputFormValues) {
+    inputFormValues = JSON.parse(inputFormValues);
+    console.log(inputFormValues);
+    Object.entries(inputFormValues).forEach(([name, value]) => {
+      // console.log(name, value);
+      formData[name] = value;
+      formFeedback.elements[name].value = value;
+    });
+  }
+}
